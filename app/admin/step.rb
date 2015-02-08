e3efb6a5210456ca0cd4cd55bf4c44f0b6b66db3
Yury Kotov <bairkan@gmail.com>
2015-02-08 16:04:52 +0400

    New step button to show page for step

--- app/admin/step.rb
+++ app/admin/step.rb
@@ -8,4 +8,8 @@ ActiveAdmin.register Step do
     :ru_help,
     :en_help
   )
+
+  action_item :new, only: :show do
+   link_to I18n.t('active_admin.new_model', model: active_admin_config.resource_label), new_resource_path
+  end
 end
