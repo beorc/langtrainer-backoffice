61e415d32c007e0cfe26bea698d5bb91c602215b
Yury Kotov <bairkan@gmail.com>
2015-02-08 16:26:56 +0400

    Added new button for unit steps show page

--- app/admin/steps_unit.rb
+++ app/admin/steps_unit.rb
@@ -21,4 +21,8 @@ ActiveAdmin.register StepsUnit do
     end
     f.actions
   end
+
+  action_item :new, only: :show do
+   link_to I18n.t('active_admin.new_model', model: active_admin_config.resource_label), new_resource_path
+  end
 end
