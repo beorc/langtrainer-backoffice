38b6dae397a45ef16b997f31ed5a5cd58c08a5f0
Yury Kotov <bairkan@gmail.com>
2015-02-22 14:17:48 +0400

    Step view improved

--- app/admin/step.rb
+++ app/admin/step.rb
@@ -9,6 +9,25 @@ ActiveAdmin.register Step do
     :en_help
   )
 
+  show do
+    attributes_table do
+      row :id
+      row :en_answers
+      row :ru_answers
+      row :ru_question
+      row :ru_question_view do
+        step.question(:ru).html_safe
+      end
+      row :ru_help
+      row :en_question
+      row :en_question_view do
+        step.question(:en).html_safe
+      end
+      row :en_help
+    end
+    active_admin_comments
+  end
+
   action_item :new, only: :show do
    link_to I18n.t('active_admin.new_model', model: active_admin_config.resource_label), new_resource_path
   end
